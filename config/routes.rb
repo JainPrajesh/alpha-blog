1405574af40e4cbd974072c537de6ffb02763999
Prajesh <jain.pra@husky.neu.edu>
2022-01-05 18:57:02 -0500

    adding article listing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'pages#home' 
   get 'about', to: 'pages#about'
+  #this will give all the routes for articles
+  #"Only" will show the show route
+  resources :articles, only: [:show, :index]
   # Defines the root path route ("/")
-  # root "articles#index"
 end
